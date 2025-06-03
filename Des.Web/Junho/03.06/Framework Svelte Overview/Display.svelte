1f05cee970a39504758c3c06a314f4feb1096e71
Ana <160749258+S-analice@users.noreply.github.com>
2025-06-03 15:23:04 -0300

    Create Display.svelte

---
+++ Des.Web/Junho/03.06/Framework Svelte Overview/Display.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let valor = '0';
+</script>
+
+<input type="text" bind:value = {valor} readonly />
+
+<style>
+  input {
+    width: 100%;
+
+    font-size: 20px;
+    text-align: right;
+
+    margin-bottom: 10px;
+    padding: 10px;
+
+    border: 2px solid hotpink;
+    border-radius: 8px;
+  }
+</style>
