c64a42a2bb86b4ed9237eff5a65963aaa1600f82
Ana <160749258+S-analice@users.noreply.github.com>
2025-06-03 15:21:44 -0300

    Create App.svelte

---
+++ Des.Web/Junho/App.svelte
@@ -0,0 +1,61 @@
+<script>
+  import Display from './Display.svelte';
+  import Keyboard from './Keyboard.svelte';
+
+  let tela = '0';
+
+  function adicionar(valor) {
+    if (tela === '0') {
+      tela = valor.toString();
+    } 
+	
+	else {
+      tela += valor.toString();
+    }
+  }
+
+  function limpar() {
+    tela = '0';
+  }
+
+  function calcular() {
+    try {
+      tela = eval(tela).toString();
+    } catch {
+      tela = 'Maybe have something wrong...';
+    }
+  }
+
+  function teclado(key) {
+    if (key === '=') {
+      calcular();
+    } 
+	
+	else if (key === 'C') {
+      limpar();
+    } 
+	
+	else {
+      adicionar(key);
+    }
+  }
+</script>
+
+<h1>Calculadora Pink</h1>
+
+<div class="calculadora">
+  <Display valor={tela} />
+  <Keyboard onButtonClick={teclado} />
+</div>
+
+<style>
+  h1 {
+    color: hotpink;
+    text-align: center;
+  }
+
+  .calculadora {
+    max-width: 300px;
+    margin: 0 auto;
+  }
+</style>
